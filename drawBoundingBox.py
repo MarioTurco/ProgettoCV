2b67ecaae093cae5eb02d2ef5bf239d2551b1377
MarioTurco <mario.turco4@studenti.unina.it>
2022-02-06 16:30:10 +0100

    Modifica script per disegnare le bounding box Per approssimare il bounding box trapezioidale in una bb rettangolare

--- drawBoundingBox.py
+++ drawBoundingBox.py
@@ -1,3 +1,4 @@
+from audioop import maxpp
 import numpy as np
 import csv 
 import cv2
@@ -11,12 +12,13 @@ y3 = []
 x4 = []
 y4 = []
 
-imagepath="Datasets/ICDAR2015/train/x/img_18.jpg"
+imagepath="Datasets/ICDAR2015/train/x/img_836.jpg"
 image = cv2.imread(imagepath)
 new_image = image
+new_image2 = image
 
 #Usa un reader CSV per estrarre le singole coordinate dal ground truth
-coordinates="Datasets/ICDAR2015/train/y/gt_img_18.txt"
+coordinates="Datasets/ICDAR2015/train/y/gt_img_836.txt"
 coords = open(coordinates, "r", encoding='utf-8-sig')
 reader = csv.reader(coords)
 for lines in reader:
@@ -31,8 +33,16 @@ for lines in reader:
 
 for i in range(0, reader.line_num):
     points = np.array([[x1[i], y1[i]], [x2[i], y2[i]], [x3[i], y3[i]], [x4[i], y4[i]]], dtype=np.int32)
-    new_image = cv2.polylines(new_image, [points], True, (256,0,256))
+    new_image = cv2.polylines(new_image, [points], True, (0,256,0))
+    # new_image2 = cv2.rectangle(new_image2, (x1[i], y1[i]), (x3[i], y3[i]), (256,0,256))    
+    # new_image2 = cv2.rectangle(new_image2, (x2[i], y2[i]), (x4[i], y4[i]), (0,0,205))
     
-cv2.imshow('Bounding Boxes', new_image)
+        
+    new_image2 = cv2.rectangle(new_image2, (min(x1[i],x2[i],x3[i],x4[i]), min(y1[i],y2[i],y3[i],y4[i])), (max(x1[i],x2[i],x3[i],x4[i]), max(y1[i],y2[i],y3[i],y4[i])), (0,0,205))    
+
+    
+#cv2.imshow('Bounding Boxes', new_image)
+cv2.imshow('Bounding Boxes', new_image2)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
